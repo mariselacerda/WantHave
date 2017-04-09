89dd04d1aaf6bc73a1aa4945b2d470ad87728fc9
mharo <mariselacerda@msn.com>
2017-04-09 09:21:18 -0500

    updated with coordinator class and added switch statement to main.

--- WantHave/Coordinator.cs
+++ WantHave/Coordinator.cs
@@ -8,12 +8,17 @@ namespace WantHave
 {
     class Coordinator
     {
+        private static List<Item>Items = new List<Item>();
         public static Item CreateItem(string itemName, string itemDescription, string itemExpiration)
         {
             var Item = new Item {ItemName = itemName, ItemDescription = itemDescription, ItemExpiration = itemExpiration };
-
-            return Item; 
+            Items.Add(Item);
+            return Item;
         }
 
+        public static List<Item> GetAllItems()
+        {
+            return Items;
+        }
     }
 }
--- WantHave/Program.cs
+++ WantHave/Program.cs
@@ -14,21 +14,38 @@ namespace WantHave
             Console.WriteLine("Use this application to give away, ask for or borrow items from people in your local community");
             Console.WriteLine(); //create some space for readability
            Console.WriteLine("Please select from the following options:");
-            Console.WriteLine("1:Give" + " 2:Borrow" + " 3:Want");
+            Console.WriteLine("1:Post A New Request" + " 2:View All Items" + " 3:View My Items" + "4:Exit");
             var option = Console.Read();
 
-            var newItem = new Item();
-            switch (option)
+            while (true)
             {
-                case "1":
-                    //collect item information
-                    break;
-                case "2":
-                    //collect item information
-                    break;
-                case "3";
-                    //collect item information
-                    break;
+                switch (option)
+                {
+                    case "1":
+                        var requesttypes = Enum.GetNames(typeof(ItemRequestType));
+                        Console.WriteLine("Select a request type:");
+                        for (var i = 0; i<(requesttypes.Length-1); i++)
+                        {
+                            Console.WriteLine($"{i}: {requesttypes[i]}");
+                        }
+                        var requesttype = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Please provide an item name:");
+                        string itemname = Console.ReadLine();
+                        Console.WriteLine("Please provide an item description:");
+                        string itemdescription = Console.ReadLine();
+                        Console.WriteLine("Please provide an item expiration:");
+                        string itemexpiration = Console.ReadLine();
+                        break;
+                    case "2":
+                        //collect item information
+                        break;
+                    case "3";
+                        //collect item information
+                        break;
+                    case "4";
+                        return;
+
+                }
             }
 
         }
